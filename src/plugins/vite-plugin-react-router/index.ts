74f2d6527404d86ec975d9348b94b5a717a0ef5c
0x219 <lzm0x219@gmail.com>
2023-05-10 15:04:59 +0800

    Update project folders

--- src/plugins/vite-plugin-react-router/index.ts
+++
@@ -1,7 +0,0 @@
-import type { PluginOption } from "vite";
-
-export function reactRouter(): PluginOption {
-  return {
-    name: "vite-plugin-react-router",
-  };
-}
---
+++ src/plugins/vite-plugin-react-routes.ts
@@ -0,0 +1,7 @@
+import type { PluginOption } from "vite";
+
+export function reactRoutes(): PluginOption {
+  return {
+    name: "vite-plugin-react-routes",
+  };
+}
